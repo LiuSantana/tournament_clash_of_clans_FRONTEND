fca98e3a279e853101e0da20a70df0d9bf0f3af7
LiuSantana <liucs02@gmail.com>
2023-11-13 10:07:32 +0100

    playoff db .ts

--- frontend/src/app/project/components/views/tournament/playoff/playoff.component.ts
+++ frontend/src/app/project/components/views/tournament/playoff/playoff.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { AWar } from 'src/app/project/services/API/war/AWar';
 
 @Component({
   selector: 'app-playoff',
@@ -7,18 +9,28 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class PlayoffComponent implements OnInit, AfterViewInit {
 
-  // temporal vars
-  rounds:any = [
-    {round: 5, wars:[1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8]},
-    {round: 4, wars:[1,2,3,4,1,2,3,4]},
-    {round: 3, wars:[1,2,1,2]},
-    {round: 2, wars:[1,1]},
-    {round: 1, wars:[1]},
-  ];
+  rounds:any = [];
   
-  constructor() { }
+  constructor(private aWar:AWar) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    let wars = await this.aWar.getAllWars().toPromise();
+    wars = wars.data;
+    wars = wars.filter((w:any) => w.format == 'playoff');
+    this.setRounds(wars);
+    
+    this.setGridColumns();
+    this.setBraketMargins(1, 10, 10);
+  }
+  setRounds(warList:any) {
+    let roundNumber = Array.from(new Set(warList.map((w:any) => w['round'].split('-')[0]))).length;
+    for(let i = 1; i < roundNumber+1; i++) {
+      let wars:any = [];
+      let roundMatches = warList.filter((w:any) => w.round.includes(`R${i}`));
+      roundMatches.sort((a: { round: string }, b: { round: string }) => a.round.localeCompare(b.round));
+      roundMatches.forEach((w:any) => wars.push(w));
+      this.rounds.push({round:roundNumber+1-i, wars:wars})
+    }
   }
 
   ngAfterViewInit(): void {
